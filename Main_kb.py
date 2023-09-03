c3e8ba6dd2b765450fe294d11c08fe983d73e879
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-09-03 21:47:35 +0300

    Update Main_kb.py

--- Main_kb.py
+++ Main_kb.py
@@ -13,8 +13,8 @@ kb_main.add(Raspisanie).insert(Svobodnii_kabi).add(Prepodi).insert(Meropriatia)
 kb_main.add(Kafedri).insert(Staga).add(Psiholog)
 
 pic_btn = [
-    InlineKeyboardButton(text='Эта'),
-    InlineKeyboardButton(text='Следующая'),
+    InlineKeyboardButton(text='Эта', callback_data='this'),
+    InlineKeyboardButton(text='Следующая', callback_data='next'),
 
 ]
 

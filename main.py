6b72e8117551df747bd124ada471ef1553d7a96e
Дмитрий Колесников <jimsgood@MacBook-Pro-Dmitrij.local>
2023-10-12 11:03:38 +0300

    Initial commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from datetime import date
 from kabs_data_and_logic import list_of_kabs_first_flour, list_of_kabs_second_flour, \
     list_of_kabs_third_flour, list_of_kabs_fourth_flour, list_of_kabs_fith_flour
 
-TOKEN_API = '6431263054:AAG5luZr2VIGwYPIiBJ4QHxEAwSKH-iil70'
+TOKEN_API = '6431263054:AAG_ybCFjORunkYDw4QLsGnoqd-ydNSBPQ0'
 
 
 bot = Bot(TOKEN_API)
